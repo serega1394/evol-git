0ec67d594f62623fff7226c13f4a9c4bee2596fe
Sergey Rozhkov <progger007@gmail.com>
2014-11-10 21:49:30 +0400

    removes comments in scanstring.php

--- scanstring.php
+++ scanstring.php
@@ -1,31 +1,5 @@
 <?php
-// This file is part of CorrectWriting question type - https://code.google.com/p/oasychev-moodle-plugins/
-//
-// CorrectWriting question type is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// CorrectWriting is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with CorrectWriting.  If not, see <http://www.gnu.org/licenses/>.
 
-
-/**
- * Defines a scanning string  options
- *
- * Abstract analyzer class defines an interface any analyzer should implement.
- * Analyzers have state, i.e. for each analyzed pair of strings there will be differrent analyzer
- *
- * @copyright &copy; 2013  Oleg Sychev
- * @author Oleg Sychev, Volgograd State Technical University
- * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
- * @package questions
- */
 define('AJAX_SCRIPT', true);
 require_once('../../../config.php');
 require_once($CFG->dirroot . '/blocks/formal_langs/block_formal_langs.php');
@@ -65,7 +39,7 @@ if ($language == null) {
             }
             $treelist = $string->tree_to_list();
             foreach($treelist as $node) {
-                /** @var block_formal_langs_ast_node_base $node */
+               
                 $string = $node->value();
                 if (is_object($string)) {
                     $string = $string->string();
